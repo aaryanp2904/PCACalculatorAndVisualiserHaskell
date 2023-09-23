fe39ffc2a29609b9a95c584c839b5bc12560cbe7
Aaryan Purohit <ap1922@ic.ac.uk>
2023-09-23 01:25:07 +0100

    Added graphing capabilities, producing a SVG image of a scatter plot displaying dataset that has gone through PCA dimensionality reduction to 2 dimensions

--- Main.hs
+++ Main.hs
@@ -3,9 +3,17 @@ import qualified Numeric.LinearAlgebra as LA
 import Data.Typeable
 import Covariance
 import Eigen
+import Graphics.Gnuplot.Simple
+import Graphics.Gnuplot.Terminal.Default
+import Control.Monad (void)
+
+import Graphics.Rendering.Chart.Easy
+import Graphics.Rendering.Chart.Backend.Diagrams
 
 pca:: [[Double]] -> [[Double]]
-pca dataPoints = res
+pca dataPoints 
+  | length dataPoints == 2 = dataPoints
+  | otherwise = res
   where covMat = getStandardizedCovarianceMatrix dataPoints
         (eigenvalsvec, eigenvecsmat) = eig covMat
         eigenvals = map realPart (toList eigenvalsvec)
@@ -15,7 +23,14 @@ pca dataPoints = res
         standardizedOriginalDataset = fromLists ([standardize col | col <- dataPoints])
         res = toLists ((removedVecsMat) LA.<> (standardizedOriginalDataset))
 
+
+scatterPlot :: [[Double]] -> Renderable()
+scatterPlot dps = toRenderable $ do
+  layout_title .= "Scatter Plot"
+  plot $ points "Data" (zip (dps !! 0) (dps !! 1))
+
 main::IO()
 main = do
-    let dataset = [[11,14,16,20,23,11,8,4,10,12,5,34,15,18,12,20,12,15,20,8], [102,132,163,201,120,110,67,195,101,124,139,120,184,95,86,211,123,153,79,115], [4,3,2,6,2,3,3,3,4,4,5,7,3,1,5,4,6,6,3,4]]
-    print(pca dataset)
+  let chart = scatterPlot
+  let transformed = pca [[10, 5, 4, 11, 6, 12, 19, 11, 20, 21, 18, 6, 19], [102,160,163,99,168,106,251,105,263,231,205,157,104], [-11, -35, -34, -9, -30, -9, 12, -8, 15, 8, 11, -31, -5]]
+  void $ renderableToFile def "scatter_plot.svg" (chart transformed)
